523576677d10a9600c7f4a9a996763c660a219c4
loveltyoic <loveltyoic@gmail.com>
2013-10-23 16:48:16 +0800

    add fork and show repo

--- app/gitcafe/git_api.rb
+++ app/gitcafe/git_api.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 module GitCafe
   class GitApi < Grape::API
     
@@ -29,17 +30,53 @@ module GitCafe
       
     end
 
-    resource :repo do
+    resource :repos do
 
       desc "Create a new repository."
-      params {requires :repo_name, :type => String}
+      params do
+        requires :repo_name, :type => String
+        requires :path, :type => String
+        requires :bare, :type => Boolean
+      end
       post '/' do
-        Grit::Repo.init("#{git_root_path}/#{params[:repo_name]}")
+        repo = GitCafe::Repo.new(params[:path], params[:repo_name], params[:bare])
+        repo.create
       end
 
+      desc 'Show a repository.'
+      params do
+        requires :repo_name, :type => String
+        requires :path, :type => String
+      end
+      get '/' do
+        repo = GitCafe::Repo.new(params[:path], params[:repo_name], false)
+        repo.show
+      end
+
+      desc 'Fork a repository.'
+      params do 
+        requires :path, :type => String
+        requires :repo_name, :type => String
+        requires :fork_path, :type => String
+      end
+      post '/:path/:repo_name/fork_bare' do
+        repo = GitCafe::Repo.new(params[:path], params[:repo_name], true)
+        repo.fork(params[:fork_path])
+      end
+
+
+      # desc "Delete a repository."
+      # params do
+      #   requires :repo_name, :type => String
+      #   requires :path, :type => String
+      # end
+      # delete '/' do 
+      #   Dir.chdir('/Users/loveltyoic/gitcafe') do
+      #     FileUtils.rm_rf("#{params[:path]}/#{params[:repo_name]}")
+      #   end
+      # end
+
     end
-
-
 
   end
 end
--- app/gitcafe/repo.rb
+++ app/gitcafe/repo.rb
@@ -1,17 +1,95 @@
 require 'grit'
 module GitCafe
   class Repo
-    def initialize(username, repo_name)
-      @repo = Grit::Repo.init("/Users/loveltyoic/gitcafe/#{username}/#{repo_name}")
+    attr_reader :repo_name, :path, :bare, :size, :result
+    def initialize(path, repo_name, bare)
+      @repo_name = repo_name
+      @path = File.join(path, repo_name)
+      @bare = bare || false
     end
 
-    def view_commits
-      @repo.commits
+    def create
+      Dir.chdir('/Users/loveltyoic/gitcafe') do
+        if File.exist?(@path) || File.exist?(@path+'.git')
+          return {
+            result: false,
+            message: 'Repo already exists.'
+          }
+        else
+          if @bare
+            @path = @path + '.git'
+            Grit::Repo.init_bare("#{@path}")
+          else
+            Grit::Repo.init("#{@path}")
+          end
+          @size = File.size(@path)
+        end
+      end
+      {
+        result: true,
+        data: {
+          name: @repo_name,
+          bare: @bare,
+          path: @path,
+          filesize: @size
+        }
+      }
     end
 
-    def self.find(username, repo_name)
-      @repo = Grit::Repo.new("/Users/loveltyoic/gitcafe/#{username}/#{repo_name}")
+    def show
+      Dir.chdir('/Users/loveltyoic/gitcafe') do
+        if File.exist?(@path) 
+          @bare = false 
+        elsif File.exist?(@path+'.git')
+          @path += '.git'
+          @bare = true
+        else
+          return {
+            result: false, 
+            message: 'Repo not exists!'
+          }
+        end
+        repo = Grit::Repo.new(@path)
+        @size = File.size(@path)
+      end
+      {
+        result: true,
+        data: {
+          name: @repo_name,
+          bare: @bare,
+          path: @path,
+          filesize: @size
+        }
+      }
     end
+
+    def fork(path)
+      Dir.chdir('/Users/loveltyoic/gitcafe') do
+        if File.exist?(@path) 
+        elsif File.exist?(@path+'.git')
+          @path += '.git'
+        else
+          return {
+            result: false, 
+            message: 'Repo not exists!'
+          }
+        end
+        repo = Grit::Repo.new(@path)
+        path = File.join(path, @repo_name+'.git')
+        repo.fork_bare(path)
+        @size = File.size(path)
+      end
+      {
+        result: true,
+        data: {
+          name: @repo_name,
+          bare: true,
+          path: path,
+          filesize: @size
+        }
+      }
+    end
+  
   end
 end
 
